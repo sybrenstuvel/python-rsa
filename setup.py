c4758893ef6adf333d0699437ee793f433100b7e
Sybren Stüvel <sybren.stuvel@chess.nl>
2008-06-06 16:04:03 +0200

    Bumped the version to 1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='rsa',
-	version='1.1',
+	version='1.2',
     description='Pure-Python RSA implementation', 
     author='Sybren Stuvel',
     author_email='sybren@stuvel.eu', 
