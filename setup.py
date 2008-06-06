e6b220bd2b636890c3badf126c3faf29c4d2f620
Sybren Stüvel <sybren@stuvel.eu>
2008-06-06 15:54:14 +0200

    Fixed UTF-8 issues

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from setuptools import setup
 
 setup(name='rsa',
 	version='1.1',
     description='Pure-Python RSA implementation', 
-    author=u'Sybren Stüvel'.encode('utf-8'), 
+    author='Sybren Stuvel',
     author_email='sybren@stuvel.eu', 
-    maintainer=u'Sybren Stüvel'.encode('utf-8'),
+    maintainer='Sybren Stuvel',
     maintainer_email='sybren@stuvel.eu',
 	url='http://www.stuvel.eu/rsa',
 	packages=['rsa'],
     license='GPL',
     classifiers=[
-        'Development Status :: 5 - Beta',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
