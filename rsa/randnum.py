bae9bbffe2f9c3269b57ba7b58775d7edad65cd3
Sybren A. Stüvel <sybren@stuvel.eu>
2011-07-23 11:01:15 +0200

    Improved random number generation

--- rsa/randnum.py
+++ rsa/randnum.py
@@ -2,7 +2,6 @@
 
 import math
 import os
-import random
 
 from rsa import common, transform
 
@@ -13,16 +12,18 @@ def read_random_int(nbits):
     resulting number can be stored in ``nbits`` bits.
     """
 
-    randomdata = os.urandom(int(math.ceil(nbits / 8.0)))
+    randomdata = os.urandom(nbits // 8)
     return transform.bytes2int(randomdata)
 
 def randint(maxvalue):
     """Returns a random integer x with 1 <= x <= maxvalue"""
 
-    readbits = max(common.bit_size(maxvalue), 32)
+    bit_size = common.bit_size(maxvalue)
+    readbits = max(bit_size, 32)
+    mask = (1 << bit_size) - 1
 
     while True:
-        value = read_random_int(readbits)
+        value = read_random_int(readbits) & mask
         if value <= maxvalue:
             return value
 
