6b6d29789a15d328bb3887ea040707aa7ca77498
Sybren A. Stüvel <sybren@stuvel.eu>
2016-03-17 13:31:43 +0100

    Bumped to version 3.4

--- rsa/__init__.py
+++ rsa/__init__.py
@@ -29,8 +29,8 @@ from rsa.pkcs1 import encrypt, decrypt, sign, verify, DecryptionError, \
     VerificationError
 
 __author__ = "Sybren Stuvel, Barry Mead and Yesudeep Mangalapilly"
-__date__ = "2016-01-13"
-__version__ = '3.4-beta1'
+__date__ = "2016-03-17"
+__version__ = '3.4'
 
 # Do doctest if we're run directly
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 if __name__ == '__main__':
     setup(name='rsa',
-          version='3.3',
+          version='3.4',
           description='Pure-Python RSA implementation',
           author='Sybren A. Stuvel',
           author_email='sybren@stuvel.eu',
