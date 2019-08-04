b68f6181e9729afc6cae42cdf12b6a8dba52a80e
Sybren A. Stüvel <sybren@stuvel.eu>
2019-08-04 17:32:23 +0200

    Added update_version.sh script
    This script updates the Python-RSA version number in various places.

---
+++ update_version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 new-version" >&2
+    exit 1
+fi
+
+DATE=$(date +'%Y-%m-%d')
+
+sed "s/__date__\s=\s'[^']*'/__date__ = '$DATE'/" -i rsa/__init__.py
+sed "s/__version__\s=\s'[^']*'/__version__ = '$1'/" -i rsa/__init__.py
+
+poetry version $1
+
+git diff
+echo
+echo "Don't forget to commit and tag:"
+echo git commit -m \'Bumped version to $1\' pyproject.toml rsa/__init__.py
+echo git tag -a version-$1 -m \'Tagged version $1\'
