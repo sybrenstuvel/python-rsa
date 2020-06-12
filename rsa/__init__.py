ec57ac07131f24156b348fda3c502ccdc9e4735b
Sybren A. Stüvel <sybren@stuvel.eu>
2020-06-12 22:33:12 +0200

    Bumped version to 4.7-dev0

--- rsa/__init__.py
+++ rsa/__init__.py
@@ -27,7 +27,7 @@ from rsa.pkcs1 import encrypt, decrypt, sign, verify, DecryptionError, \
 
 __author__ = "Sybren Stuvel, Barry Mead and Yesudeep Mangalapilly"
 __date__ = '2020-06-12'
-__version__ = '4.6'
+__version__ = '4.7-dev0'
 
 # Do doctest if we're run directly
 if __name__ == "__main__":
