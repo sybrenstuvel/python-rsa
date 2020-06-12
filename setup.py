e7c0b2a65f5f5022d4bffbd55a333d94fc5ce18f
Sybren A. Stüvel <sybren@stuvel.eu>
2020-06-12 19:52:51 +0200

    Explicitly declare Python 3.8 as supported

--- setup.py
+++ setup.py
@@ -48,10 +48,12 @@ if __name__ == '__main__':
               'Programming Language :: Python :: 3.5',
               'Programming Language :: Python :: 3.6',
               'Programming Language :: Python :: 3.7',
+              'Programming Language :: Python :: 3.8',
               'Programming Language :: Python :: Implementation :: CPython',
               'Programming Language :: Python :: Implementation :: PyPy',
               'Topic :: Security :: Cryptography',
           ],
+          python_requires='>=3.5, <4',
           install_requires=[
               'pyasn1 >= 0.1.3',
           ],
