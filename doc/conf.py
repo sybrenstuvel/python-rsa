e06a7b026819b7f44c387da0bf52c7d5f939c612
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-08-28 20:22:48 +0300

    Upgrade Python syntax with pyupgrade --py38-plus

--- doc/conf.py
+++ doc/conf.py
@@ -43,8 +43,8 @@ source_encoding = 'utf-8'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Python-RSA'
-copyright = u'2011-2019, Sybren A. Stüvel'
+project = 'Python-RSA'
+copyright = '2011-2019, Sybren A. Stüvel'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -180,8 +180,8 @@ htmlhelp_basename = 'Python-RSAdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ('index', 'Python-RSA.tex', u'Python-RSA Documentation',
-     u'Sybren A. Stüvel', 'manual'),
+    ('index', 'Python-RSA.tex', 'Python-RSA Documentation',
+     'Sybren A. Stüvel', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'python-rsa', u'Python-RSA Documentation',
-     [u'Sybren A. Stüvel'], 1)
+    ('index', 'python-rsa', 'Python-RSA Documentation',
+     ['Sybren A. Stüvel'], 1)
 ]
 
 todo_include_todos = True
