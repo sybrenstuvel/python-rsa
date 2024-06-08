fd7271182b21a4adc940a126a2a73e9f8067f596
c3equalz <dan.kovalev2013@gmail.com>
2024-06-08 17:17:36 +0300

    using sympy is_prime, it is faster than yours

--- rsa/prime.py
+++ rsa/prime.py
@@ -21,87 +21,9 @@ Roberto Tamassia, 2002.
 import rsa.helpers
 import rsa.randnum
 import math
+import sympy
 
 __all__ = ["get_prime", "are_relatively_prime"]
-
-
-def get_primality_testing_rounds(number: int) -> int:
-    """Returns minimum number of rounds for Miller-Rabing primality testing,
-    based on number bit_size.
-
-    According to NIST FIPS 186-4, Appendix C, Table C.3, minimum number of
-    rounds of M-R testing, using an error probability of 2 ** (-100), for
-    different p, q bitsizes are:
-      * p, q bit_size: 512; rounds: 7
-      * p, q bit_size: 1024; rounds: 4
-      * p, q bit_size: 1536; rounds: 3
-    See: http://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-4.pdf
-    """
-
-    # Calculate number bit_size.
-    bit_size = rsa.helpers.common.bit_size(number)
-    # Set number of rounds.
-    if bit_size >= 1536:
-        return 3
-    if bit_size >= 1024:
-        return 4
-    if bit_size >= 512:
-        return 7
-    # For smaller bitsizes, set arbitrary number of rounds.
-    return 10
-
-
-def miller_rabin_primality_testing(n: int, k: int) -> bool:
-    """Calculates whether n is composite (which is always correct) or prime
-    (which theoretically is incorrect with error probability 4**-k), by
-    applying Miller-Rabin primality testing.
-
-    For reference and implementation example, see:
-    https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
-
-    :param n: Integer to be tested for primality.
-    :type n: int
-    :param k: Number of rounds (witnesses) of Miller-Rabin testing.
-    :type k: int
-    :return: False if the number is composite, True if it's probably prime.
-    :rtype: bool
-    """
-
-    # prevent potential infinite loop when d = 0
-    if n < 2:
-        return False
-
-    # Decompose (n - 1) to write it as (2 ** r) * d
-    # While d is even, divide it by 2 and increase the exponent.
-    d = n - 1
-    r = 0
-
-    while not (d & 1):
-        r += 1
-        d >>= 1
-
-    # Test k witnesses.
-    for _ in range(k):
-        # Generate random integer a, where 2 <= a <= (n - 2)
-        a = rsa.randnum.randint(n - 3) + 1
-
-        x = pow(a, d, n)
-        if x == 1 or x == n - 1:
-            continue
-
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == 1:
-                # n is composite.
-                return False
-            if x == n - 1:
-                # Exit inner loop and continue with next witness.
-                break
-        else:
-            # If loop doesn't break, n is composite.
-            return False
-
-    return True
 
 
 def is_prime(number: int) -> bool:
@@ -115,19 +37,7 @@ def is_prime(number: int) -> bool:
     True
     """
 
-    # Check for small numbers.
-    if number < 10:
-        return number in {2, 3, 5, 7}
-
-    # Check for even numbers.
-    if not (number & 1):
-        return False
-
-    # Calculate minimum number of rounds.
-    k = get_primality_testing_rounds(number)
-
-    # Run primality testing with (minimum + 1) rounds.
-    return miller_rabin_primality_testing(number, k + 1)
+    return sympy.isprime(number)
 
 
 def get_prime(nbits: int) -> int:
