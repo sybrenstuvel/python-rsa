f13ce0ab3b06068c477e44b0736d541846eab3bf
c3equalz <dan.kovalev2013@gmail.com>
2024-06-09 18:39:35 +0300

    add some examples for library

---
+++ examples/README.md
@@ -0,0 +1,3 @@
+Thanks to NeuralNine for his review of this library
+
+Video: https://youtu.be/n0uJsqFGO4k?si=jWdeH-3HgIDPVjgK
---
+++ examples/decrypt_message.py
@@ -0,0 +1,20 @@
+import rsa
+
+
+def main() -> None:
+    with open("private.pem", "rb") as f:
+        private_key = rsa.PrivateKey.load_pkcs1(f.read())
+
+    with open("encrypted.message", "rb") as f:
+        message = f.read()
+
+    decrypted_message = rsa.decrypt(message, private_key)
+
+    print(decrypted_message.decode())
+
+    with open("decrypted.message", "wb") as f:
+        f.write(decrypted_message)
+
+
+if __name__ == "__main__":
+    main()
